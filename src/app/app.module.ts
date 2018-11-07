27a5af7abff44e4d92034fcf59fa609233d19d47
Howard Zhang <howardzhang@loop-depaulsecure-236-88.depaulsecure-student.depaul.edu>
2018-11-06 20:29:49 -0600

    Added Dashboard Component + Routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { SearchHistoryComponent } from './search-history/search-history.component';
 
 import {HistoryService } from './history.service';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 const appRoutes: Routes = [
+  { path: 'dashboard', component: DashboardComponent},
   { path: 'search-history', component: SearchHistoryComponent},
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    SearchHistoryComponent
+    SearchHistoryComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot(appRoutes),
+    FormsModule,
   ],
   providers: [HistoryService],
   bootstrap: [AppComponent]
